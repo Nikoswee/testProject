fdf15a7dfb5be305909a7ddb6a21312981dc0275
Nikoswee <nikos-wee@hotmail.com>
2023-08-20 10:41:38 +0800

    small update

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 
 import '../../widgets/widgets.dart';
 import '../screens.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+
+
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final LocalAuthentication auth;
+  bool _supportState = false;
+
+  @override
+  void initState(){
+    super.initState();
+    auth = LocalAuthentication();
+    auth.isDeviceSupported().then((bool isSupported) => setState((){
+      _supportState = isSupported;
+    }),
+    );
+  }
 
   @override
   Widget build(BuildContext context){
@@ -14,15 +36,56 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:[
-            ElevatedButton(
-              onPressed:(){
-                Navigator.push(context, MaterialPageRoute(builder:(context)=>PaymentScreen()),);
-              },
-              child: const Text('Pay')
-            ),
+                if(_supportState) 
+                  const Text('This device is supported')
+                else 
+                  const Text('This device is not supported'),
+                
+              const Divider(height: 100),
+              ElevatedButton(
+                onPressed: _getAvailableBiometrics,
+                child: const Text("Get Available Biometrics"),
+              ),
+              const Divider(height:100),
+              ElevatedButton(
+                onPressed: _authenticate,
+                child: const Text('Pay')
+              ),
           ]
         ),
       ),
     );
+  }
+
+  Future<void> _authenticate() async{
+    try{
+      List<BiometricType> availableBiometrics = await auth.getAvailableBiometrics();
+    if (availableBiometrics.contains(BiometricType.strong)){
+        bool authenticated = await auth.authenticate(
+        localizedReason: 'Please authenticate with facial recognition',
+        options: const AuthenticationOptions(
+        stickyAuth: true,
+        biometricOnly: true,
+        ),
+      );
+    print("Authenticated: $authenticated");
+
+    if (authenticated){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>PaymentScreen()));
+    }
+    }
+    } on PlatformException catch (e){
+      print(e);
+    }
+  }
+
+  Future<void> _getAvailableBiometrics() async{
+    List<BiometricType> availableBiometrics = await auth.getAvailableBiometrics();
+
+    print("List of available Biometrics: $availableBiometrics");
+
+    if(!mounted){
+      return;
+    }
   }
 }
--- lib/screens/payment/payment_screen.dart
+++ lib/screens/payment/payment_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 import '../../widgets/widgets.dart';
 import '../screens.dart';
 
-class PaymentScreen extends StatelessWidget {
+class PaymentScreen extends StatefulWidget {
+  @override
+  _PaymentScreenState createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
 
   @override
   Widget build(BuildContext context){
@@ -14,15 +20,15 @@ class PaymentScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:[
+              const Divider(height:50),
               Text('Succesfully paid!'),
-              ElevatedButton(
-              onPressed:(){
-                Navigator.push(context, MaterialPageRoute(builder:(context)=>HomeScreen()),);
-              },
-              child: const Text('BACK')
-            ),
-
-          ]
+                ElevatedButton(
+                  onPressed:(){
+                    Navigator.push(context, MaterialPageRoute(builder:(context)=>HomeScreen()),);
+                  },
+                  child: const Text('BACK')
+                )
+          ],
         ),
       ),
     );
